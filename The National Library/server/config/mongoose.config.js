8a79e0258da0df7c9ff1c682bff819f28011dbe6
Alaa ibrahim <81506835+Adeenib@users.noreply.github.com>
2021-08-10 11:16:13 +0300

    ADD( useCreateIndex: true) in config file

--- The National Library/server/config/mongoose.config.js
+++ The National Library/server/config/mongoose.config.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/NLibrary", {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
+	useCreateIndex: true
 })
 	.then(() => console.log("Established a connection to the database"))
 	.catch(err => console.log("Something went wrong when connecting to the database", err));

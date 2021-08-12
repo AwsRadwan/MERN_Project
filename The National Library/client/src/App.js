e834f7421ce5ff9eda15b7673d6853b3724fb9fe
Abdulrahman-ghouli <a.e.ghouli@gmail.com>
2021-08-12 11:04:08 +0300

    update

--- The National Library/client/src/App.js
+++ The National Library/client/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import Admin from './views/Admin';
+// import Admin from './views/Admin';
+import CommentForm from './components/CommentForm';
 function App() {
   return (
     <div className="App">
       <Admin />
+      
     </div>
   );
 }
